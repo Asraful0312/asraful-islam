d4fbce1036d904b1e3e4ade8f020b13df316b9d4
asraful islam <asrafulislamrayhan2@gmail.com>
2025-04-18 22:19:58 +0600

    updated contact information

--- components/tags.tsx
+++ components/tags.tsx
@@ -9,6 +9,33 @@ type Tag = string;
 
 const tags = ["React"];
 
+type PortalFormProps = {
+  questions: {
+    id: string;
+    question: string;
+    answered: string | null;
+  }[];
+  type: "Appointment" | "Payment";
+  customerId: string;
+  domainid: string;
+  email: string;
+  bookings?:
+    | {
+        date: Date;
+        slot: string;
+      }[]
+    | undefined;
+  products?:
+    | {
+        name: string;
+        image: string;
+        price: number;
+      }[]
+    | undefined;
+  amount?: number;
+  stripeId?: string;
+};
+
 export default function Tags() {
   const id = useId();
   const [exampleTags, setExampleTags] = useState<Tag[]>(tags);
